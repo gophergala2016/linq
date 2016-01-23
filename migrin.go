5fe18409800088d655628452af2b3814810ed718
urielhdz <uriel.hdzc@gmail.com>
2016-01-23 11:19:32 -0600

    New migrations can be created

---
+++ migrin.go
@@ -0,0 +1,61 @@
+package main
+
+import(
+	"fmt"
+	"gopkg.in/alecthomas/kingpin.v2"
+	"time"
+	"os"
+	"log"
+	"bufio"
+)
+
+var (
+	action = kingpin.Arg("action","Specify an action to run init/new/up/down").Required().String()
+	option = kingpin.Arg("modifier","Adds extra information to the command, specifies the migration name on new command").String()
+)
+
+type Migrin struct{
+
+}
+
+func (this Migrin) new() {
+	if *option == ""{
+		fmt.Println("Missing migration name.")
+		return
+	}else{
+		t := time.Now()
+		timestamp := t.Format("20060102150405")
+		this.create_file(timestamp,*option)
+	}	
+}
+
+func (this Migrin) create_file(timestamp,filename string) {
+	go this.create_migrations_table()
+	f,err := os.Create(timestamp+"_"+filename+".go")
+	if err != nil{
+		log.Fatal(err)
+	}
+	w := bufio.NewWriter(f)
+
+	_,err = w.WriteString("package main \n\n import(\n\t 'fmt' \n)\n\n func main(){}")
+	
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	f.Sync()
+	w.Flush()
+}
+
+func (this Migrin) create_migrations_table() {
+	
+}
+
+func main() {
+	kingpin.Parse()
+	m := Migrin{}
+	switch *action{
+		case "new":
+			m.new()
+	}
+}
